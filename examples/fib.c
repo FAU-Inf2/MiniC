68797be097616e9487e144bad5b781bc12be69c6
Patrick Kreutzer <patrick.kreutzer@fau.de>
2021-10-06 13:40:14 +0200

    Add grammar and fibonacci example.

---
+++ examples/fib.c
@@ -0,0 +1,17 @@
+int fib_h(int n, int a, int b) {
+  if (n == 0) {
+    return a;
+  }
+  if (n == 1) {
+    return b;
+  }
+  return fib_h(n - 1, b, a + b);
+}
+
+int fib(int n) {
+  return fib_h(n, 0, 1);
+}
+
+int main() {
+  return fib(8);
+}
