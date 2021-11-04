4c8e36867abce65bd9107c6d744ceb6246d063ef
Patrick Kreutzer <patrick.kreutzer@fau.de>
2021-11-04 13:05:22 +0100

    Add basic test script for crashes.

---
+++ checks/crash.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+readonly TIMEOUT=10 # seconds
+
+readonly THIS_DIR="$(dirname "$0")"
+readonly MINIC="$THIS_DIR/../run.sh"
+
+if [[ $# -lt 1 || ! -f "$1" ]] ; then
+  echo "USAGE: $0 <program path>" >&2
+  exit 0 # sic
+fi
+
+readonly INPUT_PROGRAM="$1"
+shift
+
+timeout "$TIMEOUT" "$MINIC" --in "$INPUT_PROGRAM" --interpret $@ 2> /dev/null > /dev/null
+exit_code="$?"
+
+if [ "$exit_code" -eq 124 ] ; then
+  # timeout
+  exit 0
+fi
+
+if [ "$exit_code" -eq 1 ] ; then
+  exit 1 # crash
+else
+  exit 0 # no crash
+fi
